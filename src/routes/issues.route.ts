abdf2bb79c6c78ac985a839d49981c9d1ffd7684
JosephSuyam <josephsuyam@gmail.com>
2024-03-10 15:51:52 +0800

    feat: added endpoints for fetching issues

--- src/routes/issues.route.ts
+++ src/routes/issues.route.ts
@@ -5,6 +5,8 @@ import IssuesService from '../service/issues';
 const issuesRouter = Router();
 const issueService = new IssuesService();
 
+issuesRouter.get('/', issueService.getIssues);
+issuesRouter.get('/:id', issueService.getIssue);
 issuesRouter.post('/', addIssuesValidator(), issueService.createIssues);
 
 export default issuesRouter;
--- src/service/issues.ts
+++ src/service/issues.ts
@@ -1,8 +1,32 @@
 import { Request, Response } from 'express';
+import { eq } from 'drizzle-orm';
 import { db } from '../database';
 import { issues } from '../models/issues.model';
 
 export default class IssuesService {
+  async getIssues(req: Request, res: Response): Promise<Response> {
+    try {
+      const data = await db.select().from(issues);
+
+      return res.status(200).json({ success: true, data: data });
+    } catch (error) {
+      console.log('getIssues error:', error);
+      return res.status(500).json({ success: false, message: error });
+    }
+  };
+
+  async getIssue(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
+      const data = await db.select().from(issues).where(eq(issues.id, Number(id)));
+
+      return res.status(200).json({ success: true, data });
+    } catch (error) {
+      console.log('getIssue error:', error);
+      return res.status(500).json({ success: false, message: error });
+    }
+  };
+
   async createIssues(req: Request, res: Response): Promise<Response> {
     try {
       const issue = await db.insert(issues).values(req.body).returning();
@@ -18,4 +42,3 @@ export default class IssuesService {
     }
   };
 }
-
